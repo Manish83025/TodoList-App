d2900da4a4504ef7d5076410342d48db940994cf
Manish83025 <manishmahawarofficial@gmail.com>
2025-02-17 12:24:26 +0530

    Add Project

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+import Navbar from './components/Navbar'
+
+function App() {
+  
+ 
+   
+
+  return (
+    <>
+    <Navbar/>
+    <div className="container mx-auto">
+      <div className="bg-red-600">
+      <h1>Your Todos</h1>
+      </div>
+    </div>
+      
+
+      
+    </>
+  )
+}
+
+export default App
